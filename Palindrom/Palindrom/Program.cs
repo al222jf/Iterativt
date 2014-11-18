4cb6d1cc811f7d865faf603c263d96698b851332
al222jf <al222jf@student.lnu.se>
2014-11-18 18:17:33 +0100

    Uppgift 3a

---
+++ Palindrom/Palindrom/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = Console.ReadLine();
+            string reversed = Reverse(value);
+
+            string a = (string) value;
+            string b = (string) reversed;
+
+            if (a == b)
+            {
+                Console.WriteLine("{0} = True", value);
+            }
+            else
+            {
+                Console.WriteLine("{0} = False", value);
+            }
+        }
+        public static string Reverse(string str)
+        {
+            char[] CharArray = str.ToCharArray();
+            string reverse = null;
+            for(int i = CharArray.Length - 1; i > -1; i--)
+            {
+                reverse += CharArray[i];
+            }
+            return reverse;
+        }
+    }
+}
