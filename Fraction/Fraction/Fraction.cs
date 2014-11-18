a1c8e90872bd6ccd52dcd4d5ecf094cc2c2cee6e
al222jf <al222jf@student.lnu.se>
2014-11-19 00:39:06 +0100

    end of day commit

---
+++ Fraction/Fraction/Fraction.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    class Fraction
+    {
+        int _numerator;
+        int _denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            getNumerator = numerator;
+            getDenominator = denominator;
+
+
+        }
+
+        public int getNumerator
+        {
+            get { return _numerator; }
+            set 
+            {
+                _numerator = value;
+            }
+
+        }
+
+        public int getDenominator
+        {
+            get { return _denominator; }
+            set 
+            {
+                _denominator = value;
+            }
+
+        }
+
+        public bool isNegative()
+        {    
+            if( _numerator < 0 || _denominator < 0){
+                return true;
+
+            } 
+            else
+            {
+                return false;
+            }
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+
+        }
+
+        public multiply(){
+
+        }
+
+        public isEqualTo(){
+
+        }
+
+        public toSting(){
+
+        }
+    }
+}
---
+++ Fraction/Fraction/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int _numerator;
+            int _denominator;
+            
+
+            Console.Write("Ange täljaren: ");
+            _numerator = int.Parse(Console.ReadLine());
+        }
+    }
+}
